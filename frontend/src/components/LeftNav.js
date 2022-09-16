b4bb7fc1fee66e226623cbe1fd139b702db14735
ravi <ravitokas.9000@gmail.com>
2022-09-17 01:20:26 +0400

    first commit

---
+++ frontend/src/components/LeftNav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftNav = () => {
+  return <div>Left Navigation</div>;
+};
+
+export default LeftNav;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>App Home page</div>;
+};
+
+export default Home;
